241132b2a5e7d8dc6ac26ae9256c51c9ae4bd95c
muhmd-ahmed <momo200200mm@gmail.com>
2022-02-21 20:18:25 +0200

    final modify 03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { OverlayComponent } from './layouts/overlay/overlay.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    OverlayComponent
+    OverlayComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
