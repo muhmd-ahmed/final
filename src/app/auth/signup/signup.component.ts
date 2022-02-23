6e70b684d018202c52e5911cce730b8b4519f6b9
mahm0ud-abdelhalim <mahmoud.abdelhalim.mostafa@outlook.com>
2022-02-23 11:26:10 +0200

    error msgs

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -9,7 +9,7 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent implements OnInit {
 
   signup(form:NgForm){
-    console.log(form);
+    console.log(form.value);
     
   }
   constructor() { }
